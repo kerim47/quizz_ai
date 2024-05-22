ef3b08b0465cb06701e2b9f658cf325975507c11
Abdulkerim <kerimakan77@gmail.com>
2024-05-22 19:35:01 +0300

    fix: bottom overflowed pixel hatasi giderildi

--- lib/features/user_auth/presentation/pages/landing_page.dart
+++ lib/features/user_auth/presentation/pages/landing_page.dart
@@ -28,6 +28,7 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         leading: Builder(builder: (BuildContext context) {
           return IconButton(
